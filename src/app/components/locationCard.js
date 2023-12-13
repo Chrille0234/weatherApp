62396fc31ff371439bf01fdf507e0e5838bfc665
chrille0234 <madsen.chr@hotmail.com>
2023-12-13 13:38:16 +0100

    initial commit

---
+++ src/app/components/locationCard.js
@@ -0,0 +1,9 @@
+export default function LocationAndCurrentWeather(props){
+    return (
+        <article>
+            <h2>{props.city}</h2>
+            <div>{props.degrees}</div>
+            <div>hej</div>
+        </article>
+    )
+}
