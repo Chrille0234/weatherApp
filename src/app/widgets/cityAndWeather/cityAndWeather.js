0c6847621bcdbfcb0558d202d88f3a8b17a9c32a
chrille0234 <madsen.chr@hotmail.com>
2023-12-18 10:16:35 +0100

    change l and h to highest and lowest in

--- src/app/widgets/cityAndWeather/cityAndWeather.js
+++ src/app/widgets/cityAndWeather/cityAndWeather.js
@@ -1,11 +1,11 @@
 export default function WeatherWidget({object}){
-    let {city, celsius, clouds, latitude, longtitude} = object
+    let {city, celsius, clouds, maxTemp, minTemp} = object
     return (
             <div className="text-white text-center h-[50%] w-full flex flex-col items-center justify-center">
                 <h2 className="text-3xl">{city}</h2>
                 <div className="text-6xl">{celsius}°</div>
                 <div className="opacity-50 text-xl capitalize font-bold">{clouds}</div>
-                <span className="font-bold">H: {Math.floor(latitude)}° L: {Math.floor(longtitude)}°</span>
+                <span className="font-bold">H: {Math.floor(maxTemp)}° L: {Math.floor(minTemp)}°</span>
             </div>
     )
 }
